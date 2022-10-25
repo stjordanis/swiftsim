16aaca3fa3ee26c1cc0e382874e73bee1f9a0ee0
John Helly <j.c.helly@durham.ac.uk>
2022-10-25 17:09:07 +0100

    Include weight in neutrino maps where necessary

--- src/neutrino/Default/neutrino.c
+++ src/neutrino/Default/neutrino.c
@@ -383,7 +383,13 @@ double lightcone_map_neutrino_mass_get_value(
 
   switch (gp->type) {
     case swift_type_neutrino: {
-      return gp->mass;
+      struct neutrino_model nu_model;
+      bzero(&nu_model, sizeof(struct neutrino_model));
+      if (e->neutrino_properties->use_delta_f_mesh_only)
+        gather_neutrino_consts(e->s, &nu_model);
+      double weight = 1.0;
+      gpart_neutrino_weight_mesh_only(gp, &nu_model, &weight);
+      return gp->mass*weight;
     } break;
     default:
       error("lightcone map function called on wrong particle type");
